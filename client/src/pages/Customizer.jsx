9cd9373b0d4c487c4d6c0929d46fc86d9f947652
Velizar Dabov <velizardabov@gmail.com>
2024-05-14 12:33:20 +0300

    customizer ready

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,13 +1,57 @@
-import React,{useState, useEffect} from 'react'
-import { AnimatePresence, motion } from 'framer-motion';
-import { useSnapshot } from 'valtio';
-import state from '../store';
-import { downloadCanvasToImage, reader } from '../config/helpers';
-
+import React, { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import { EditorTabs, FilterTabs } from "../config/constants";
+import Tab from "../components/Tab";
+import CustomBtn from "../components/CustomBtn";
 const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Customizer</div>
-  )
-}
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10 "
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomBtn
+              title="Go Back"
+              type="filled"
+              handleClick={() => (state.intro = true)}
+              customStyle="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+          <motion.div className="filtertabs-container" {...slideAnimation}>
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
 
-export default Customizer
+export default Customizer;
